54c4463e07800ff6dde457af9befeb7381014e14
Mayu Miyoshi <mayu.miyoshi1313@gmail.com>
2021-01-25 22:19:33 +0900

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  TYPE = ['business', 'intern']
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :submissions # bookings as a client
   has_many :positions
   has_many :submissions_as_owner, through: :positions, source: :submissions
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :type, presence: true
+  validates :description, presence: true
+  validates :type, inclusion: { in: TYPE, message: "Please select business or intern"}
 end
