922c1f75dbd3c3c46e25bb130739b5375fb4037e
Chii Law <cyee3012@hotmail.co.uk>
2021-01-28 21:06:51 +0900

    added actions in submissions_controller. also added routes for submissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'positions#index'
 
-  resources :positions, only: [ :index, :show]
+  resources :positions, only: [ :index, :show] do
+    resources :submissions, only: [ :new, :create]
+  end
+
+  resources :submissions, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
